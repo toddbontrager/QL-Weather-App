91020855fc8cce6e11bee8d2722137a261cff5ee
Todd Bontrager <bontragertodd@gmail.com>
2016-06-25 11:16:58 -0400

    Add basic middleware for parsing and serving static files

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 
 var app = express();
 
-require('./server/utils/middleware.js')(app, express);
-require('./server/routes')(app, express);
+require('./server/middleware.js')(app, express);
+require('./server/routes.js')(app, express);
 module.exports = app;
